7c897a40c6b59b195a0db0be821a6081ce480c7e
sovsianykov <swebproger@gmail.com>
2021-01-02 22:02:51 +0200

    Revert "new year 4"
    This reverts commit 928cb47e48c7d18335992d83b181cedd49318053.

--- src/Components/MonthItems/ArrayOfAllMonthes/ArrayOfAllMonths.js
+++ src/Components/MonthItems/ArrayOfAllMonthes/ArrayOfAllMonths.js
@@ -4,7 +4,7 @@ import Aoux from "../../../Houk/Aoux";
 const ArrayOfAllMonths = (props) => (
   <Aoux>
     <tr>
-      <th>{props.name}</th>
+      <td>{props.name}</td>
       <td className='pay'>{props.elPay} </td>
       <td>{props.elDebt}</td>
       <td>{props.elDate}</td>
